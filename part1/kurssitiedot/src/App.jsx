1740ed7eb36540b3af75ef7f452583e2f0639cd7
Laura Bukonte <laura_bukonte@Lauras-MacBook-Air.local>
2024-03-04 22:08:49 +0200

    Part1, kurssitiedot, step 2

--- part1/kurssitiedot/src/App.jsx
+++ part1/kurssitiedot/src/App.jsx
@@ -27,18 +27,25 @@ const Header = (props) => {
   )
 }
 
+
+
+
 const Content = (props) => {
-  console.log(props)
+  return (
+    <div>
+      <Part part= {props.part1} exercises= {props.exercises1}/>
+      <Part part= {props.part2} exercises= {props.exercises2}/>
+      <Part part= {props.part3} exercises= {props.exercises3}/>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  console.log("Part component:", props)
   return (
     <div>
       <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
+        {props.part} {props.exercises}
       </p>
     </div>
   )
